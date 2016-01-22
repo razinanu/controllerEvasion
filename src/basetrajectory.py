5688fa58df381334b5c4db4ab6097cbb9f73aa15
Razi Nanu <razi.ghassemi@gmail.com>
2016-01-22 22:52:19 +0100

    Trajectory with while loop optimization

--- src/basetrajectory.py
+++ src/basetrajectory.py
@@ -6,20 +6,28 @@ import matplotlib.pyplot as plt
 
 
 class BaseTrajectory:
-
-    def __init__(self):
-
-        self.inflection_error = 0.005
-        self.flat_error = 0.001
     """
     Parameters
     ----------
-    config : array
+    trajectory_config : array
     config[0] : lateral_length
     the lateral length of trajectory
     config[1] : velocity
     config[2] : lateral_acceleration
     maximum lateral Acceleration of vehicle
+    """
+    def __init__(self, trajectory_config):
+
+        self.inflection_error = 0.005
+        self.flat_error = 0.001
+        self.lateral_length = trajectory_config[0]
+        self.velocity = trajectory_config[1]
+        self.lateral_acceleration = trajectory_config[2]
+        self.longitudinal = self.velocity * (math.sqrt((math.pi * self.lateral_length) / self.lateral_acceleration))
+    """
+    Parameters
+    ----------
+    start_point: Start point of trajectory
     fraction: float
     set the fraction of trajectory (max=1)
     velocity : float
@@ -31,29 +39,24 @@ class BaseTrajectory:
     -------
     trajectory : array
     """
-    def lane_change_base_trajectory(self, config, start_point, fraction, step):
+    def lane_change_base_trajectory(self, start_point, fraction, step):
 
-        lateral_length = config[0]
-        velocity = config[1]
-        lateral_acceleration = config[2]
-        longitudinal = velocity * (math.sqrt((math.pi * lateral_length) / lateral_acceleration))
         inflection_point = np.zeros(shape=(2, 2), dtype=float)
         flat_part = np.zeros(shape=(1, 2), dtype=float)
         trajectory = np.zeros(shape=(step, 2), dtype=float)
-        y = lateral_length / 2 * np.pi
-        z = (2 * np.pi) / longitudinal
+        y = self.lateral_length / 2 * np.pi
+        z = (2 * np.pi) / self.longitudinal
         c = 0
         j = 0
         t = 0
         """ start_point of trajetory  """
-        for i in np.linspace(longitudinal*start_point, longitudinal*fraction, step):
+        for i in np.linspace(self.longitudinal*start_point, self.longitudinal*fraction, step):
             trajectory[c, 0] = i
             trajectory[c, 1] = y*z*i - y * np.sin(z * i)
-           # print "trajectory x: ", trajectory[c, 0], "trajectory y: ", trajectory[c, 1]
-            if np.abs(longitudinal/2-i) < self.inflection_error and j < 2:
+
+            if np.abs(self.longitudinal/2-i) < self.inflection_error and j < 2:
                 inflection_point[j, 0] = trajectory[c, 0]
                 inflection_point[j, 1] = trajectory[c, 1]
-           # print "inflection x: ",self.inflection_point[j, 0], "inflection y: ", self.inflection_point[j, 1]
                 j += 1
 
             if(np.abs(trajectory[c, 1]-trajectory[c-1, 1]) < self.flat_error) and c > 0:
@@ -62,31 +65,38 @@ class BaseTrajectory:
                 flat_part[t, 1] = trajectory[c, 1]
                 t += 1
             c += 1
-        # plt.plot(trajectory[:, 0], trajectory[:, 1], 'r')
+        #
+        #  plt.plot(trajectory[:, 0], trajectory[:, 1], 'r')
         # plt.plot(inflection_point[:, 0], inflection_point[:, 1], 'go')
         # plt.plot(flat_part[:, 0], flat_part[:, 1], 'bo')
         return trajectory
 
-    def find_flat_part(self, tajectory):
+    def trajectory_function(self, x):
+            y = self.lateral_length / 2 * np.pi
+            z = (2 * np.pi) / self.longitudinal
+            function = y*z*x - y * np.sin(z * x)
+            plt.plot(x, function, 'r')
+            return function
+
+    def find_flat_part(self, trajectory):
 
         flat_point = np.array([0, 0], dtype=float)
 
         for i in range(0, trajectory.shape[0]):
             if np.abs(trajectory[i, 1]-trajectory[i+1, 1]) > self.flat_error:
                 flat_point[0] = trajectory[i, 0]
-                print "x t :", trajectory[i, 0]
                 flat_point[1] = trajectory[i, 1]
-                print "x is ", flat_point[0]
-                print "y is ", flat_point[1]
+
                 break
         return flat_point
 if __name__ == '__main__':
 
     conf = np.array([0.4, 3, 2])
-    trajectory = BaseTrajectory().lane_change_base_trajectory(conf, 0, 1, 300)
+    trajectory = BaseTrajectory(conf).lane_change_base_trajectory(0, 1, 300)
     plt.plot(trajectory[:, 0], trajectory[:, 1], 'r')
-    flat_point = BaseTrajectory().find_flat_part(trajectory)
-    plt.plot(flat_point[0], flat_point[1], 'go')
+    plt.show()
+    x = np.linspace(0, BaseTrajectory(conf).longitudinal)
+    BaseTrajectory(conf).trajectory_function(x)
     plt.show()
 
 
--- src/trajectory.py
+++ src/trajectory.py
@@ -7,35 +7,80 @@ import basetrajectory
 
 class Trajectory:
 
-    def __init__(self):
+    def __init__(self, base_trajectory_config):
 
         self.model = OdeModel.OdeModel()
         self.step = 30
         self.moment_max = 22.5
         self.servo_max = 0.35
+        self.base_trajectory = basetrajectory.BaseTrajectory(base_trajectory_config)
+        self.min_velocity =5
+        self.max_velocity = 23
+        self.min_servo = 1
+        self.max_servo = 3
+        self.min_step=5
 
-    def lane_change_trajectory(self, conf_start, base_trajectory, start_servo, start_moment,flat_point):
+    def lane_change_trajectory(self, conf_start, start_servo, start_moment):
 
-        solution = self.model.solve(x_start=conf_start, u=(0, 2),
-                                    n_steps=2, dt=0.001)
+        step = self.min_step
+        solution = self.model.solve(x_start=conf_start, u=(0.0, 5),
+                                    n_steps=step, dt=0.001)
+        function = self.base_trajectory.trajectory_function(solution[:, 0])
 
-        while flat_point[0] > solution[-1, 0]:
-            solution = self.model.solve(x_start=conf_start, u=(0, self.moment_max), n_steps=self.step, dt=0.001)
-            plt.plot(solution[:, 0], solution[:, 1], 'go')
-            conf_start = np.array([solution[-1, 0], solution[-1, 1],
-                                   solution[-1, 2], solution[-1, 3], solution[-1, 4], solution[-1, 5], solution[-1, 6]])
+        x = 0.88*step
+        v = self.min_velocity
+        u = self.min_servo
+        velocity_max = 0
+        max_servo = 0
+        max_x = 0
+        max_step = 0
+        while(u < self.max_servo):
+
+            while(v < self.max_velocity):
+
+                while np.abs(solution[x:, 1].mean(0) - function[x:].mean(0)) < 0.0001:
+                    step += 90
+                    solution = self.model.solve(x_start=conf_start, u=((u/10), v),
+                                     n_steps =step, dt=0.001)
+                    function = self.base_trajectory.trajectory_function(solution[:, 0])
+                    plt.plot(solution[:, 0], solution[:, 1], 'go')
+                    if(max_x < solution[-1,0]):
+                         velocity_max = v
+                         max_servo= u
+                         max_step=step
+                         max_x= solution[-1, 0]
+                         bestTryjectory= np.array(solution.shape, dtype=float)
+                         bestTryjectory=solution
+
+                v += 1
+                step=5
+                solution = self.model.solve(x_start=conf_start, u=(u, v),
+                                    n_steps=step, dt=0.001)
+                function = self.base_trajectory.trajectory_function(solution[:, 0])
+            step=5
+            v=10
+            u += 0.1
+            solution = self.model.solve(x_start=conf_start, u=(u, v),
+                                    n_steps=step, dt=0.001)
+            function = self.base_trajectory.trajectory_function(solution[:, 0])
 
 
-        plt.plot(base_trajectory[:, 0], base_trajectory[:, 1], 'r')
+        print "max is", max_x
+        print "max_servo", max_servo
+        print "max_vel", velocity_max
+        print "max_step",max_step
+
+        plt.plot(bestTryjectory[:, 0], bestTryjectory[:, 1], 'bo')
         return solution
 if __name__ == '__main__':
 
     conf = np.array([0.4, 3, 2])
-    base_trajectory = basetrajectory.BaseTrajectory().lane_change_base_trajectory(conf, 0, 0.5, 300)
-    flat_point = basetrajectory.BaseTrajectory().find_flat_part(base_trajectory)
+    #base_trajectory = basetrajectory.BaseTrajectory(conf).lane_change_base_trajectory(0, 0.5, 300)
+    #flat_point = basetrajectory.BaseTrajectory(conf).find_flat_part(base_trajectory)
     conf_start = np.array([0, 0, 0, 0, 0, 1, 0])
     #u=(0.35, 22.5)
-    solution = Trajectory().lane_change_trajectory(conf_start, base_trajectory, 0, 22.5,flat_point)
+    solution = Trajectory(conf).lane_change_trajectory(conf_start, 0, 5)
+
     plt.show()
 
 
